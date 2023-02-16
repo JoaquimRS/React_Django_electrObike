91fc17d81f8afd57884b9acfdbd8b4e1a8f597af
JoaquimRS <joaquimdaweb@gmail.com>
2023-02-16 20:45:14 +0100

    (frontend) - ClientNotifications

--- frontend/src/pages/Notification/Notification.jsx
+++ frontend/src/pages/Notification/Notification.jsx
@@ -1,9 +1,30 @@
+import {NotificationItem} from "../../components";
+import Refresh from "../../assets/icons/Refresh";
+import "./Notification.scss"
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 const Notification = () => {
+    const authClient = useSelector(state => state.auth.user)
+    const [client, setClient] = useState(null)
+   
+    useEffect(() => {
+        setClient(authClient)
+    }, [authClient])
 
     return (
-        <div className="notification">
-            <h1>Notification</h1>
+        <div className="notifications">
+            <div className="header">
+                <h2>Centro de notificaciones</h2>
+                <div className="refresh-icon">
+                    <Refresh/>
+                </div>
+            </div>
+            <div className="items">
+            {client 
+                ? client.notifications.map((notification, index) => (<NotificationItem key={index} notification={notification}/>)) 
+                : null}
+            </div>
         </div>
     )
 }
